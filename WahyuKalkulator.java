41d4d6189a207b02f2257b29044b0d7255ace2b9
Wahyu Satrio Wibowo <wahyuusatriowibowo@gmail.com>
2024-12-18 19:12:42 +0700

    Add files via upload

---
+++ WahyuKalkulator.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class WahyuKalkulator {
+
+    public static void main(String[] args) {
+        Scanner inputUser = new Scanner(System.in);
+        float a, b, hasil;
+        char operator;
+
+        // Input nilai a
+        System.out.print("Masukan Nilai Pertaman Kamu : ");
+        a = inputUser.nextFloat();
+
+        // Input operator
+        System.out.print("Operator (+, -, *, /): ");
+        operator = inputUser.next().charAt(0);
+
+        // Input nilai b
+        System.out.print("Masukan Nilai Ke Dua Kamu: ");
+        b = inputUser.nextFloat();
+
+        // Menampilkan input dari pengguna
+        System.out.println("Input user: " + a + " " + operator + " " + b);
+
+        // Penghitungan berdasarkan operator
+        if (operator == '+') {
+            hasil = a + b;
+            System.out.println("Hasil = " + hasil);
+
+        } else if (operator == '-') {
+            hasil = a - b;
+            System.out.println("Hasil = " + hasil);
+
+        } else if (operator == '*') {
+            hasil = a * b;
+            System.out.println("Hasil = " + hasil);
+
+        } else if (operator == '/') {
+            if (b != 0) {
+                hasil = a / b;
+                System.out.println("Hasil = " + hasil);
+
+            } else {
+                System.out.println("Kesalahan : Pembagian dengan nol tidak diperbolehkan.");
+            }
+        } else {
+            System.out.println("Operator hasil yang anda input tidak ditemukan.");
+        }
+    }
+}
